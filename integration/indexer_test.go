2273ef48627c6d4610e63d0d6ff39fe64ede0f43
Kevin Stiehl <kevin.stiehl@numericas.de>
2022-08-28 23:34:26 +0200

    add integration test structure

---
+++ integration/indexer_test.go
@@ -0,0 +1,14 @@
+package integration_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	// . "github.com/onsi/gomega"
+)
+
+
+var _ = Describe("Indexer", func(){
+
+	It("this and that", func(){
+	
+	})
+})
---
+++ integration/suite_test.go
@@ -0,0 +1,15 @@
+package integration_test
+
+import (
+	"testing"
+      . "github.com/onsi/ginkgo/v2"
+      . "github.com/onsi/gomega"
+)
+
+
+func TestSuite(t *testing.T) {
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Integration Tests")
+
+}
